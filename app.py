23304e23b49dab7812e98332c9f0f80875f4287c
lia-defi <94789035+lia-defi@users.noreply.github.com>
2021-12-06 20:19:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from multiapp import MultiApp
 import quantapp
 import home
 import supportme
+import machlearn
 # import your app modules here
 
 from PIL import Image
@@ -19,6 +20,7 @@ st.markdown("""
 app.add_app("Home", home.app)
 app.add_app("Statistical Arbitrage", quantapp.app)
 app.add_app('Support Me',supportme.app)
+app.add_app("Machine Learning for Stocks Prediction",machlearn.app)
 
 # The main app
 app.run()
