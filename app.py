e9746d1937b50c3d49b7c56a8d74c947b1ff18a1
lia-defi <94789035+lia-defi@users.noreply.github.com>
2021-12-08 14:05:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.markdown("""
 app.add_app("Home", home.app)
 app.add_app("Statistical Arbitrage", quantapp.app)
 app.add_app('Support Me',supportme.app)
-app.add_app("Machine Learning for Stocks Prediction",machlearn.app)
+#app.add_app("Machine Learning for Stocks Prediction",machlearn.app)
 
 # The main app
 app.run()
