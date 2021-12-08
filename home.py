6fe9f14c4e54ac72bc4c17ac58f4942abcdae8bc
lia-defi <94789035+lia-defi@users.noreply.github.com>
2021-12-09 00:33:10 +0100

    Update home.py

--- home.py
+++ home.py
@@ -4,3 +4,7 @@ def app():
     st.title('Welcome')
 
     st.write('To this page. The Smirk is a Web page App that allows user, that doesn’t have a particular coding skill, to do backtesting their strategy')
+    
+    link = '[TheSmirk](https://thesmile.herokuapp.com/)'
+    st.subheader("Second Part:")
+    st.markdown(link, unsafe_allow_html=True)
