7d0b14ef7365292e1b4fc3c4a70b673dccff600f
lia-defi <94789035+lia-defi@users.noreply.github.com>
2021-12-08 16:18:52 +0100

    Update supportme.py

--- supportme.py
+++ supportme.py
@@ -5,8 +5,4 @@ def app():
 
     img = Image.open('payme.jpeg')
     st.image(img,width=200,caption='Scan to support this project')
-    st.subheader("What I’m going to do with your donation: 
-    "-  Implementing new models"
-    "-	Buy better servers to make faster analysis
- 
-    ")
+    st.subheader("What I’m going to do with your donation: implementing new models, buy better servers to make faster analysis")
