8f592b9baf5188d0cbbc6760ef097696ff1e8280
lia-defi <94789035+lia-defi@users.noreply.github.com>
2021-12-08 16:23:34 +0100

    Update home.py

--- home.py
+++ home.py
@@ -3,4 +3,4 @@ import streamlit as st
 def app():
     st.title('Welcome')
 
-    st.write('To this page. The Smirk is a Web page App that allows users, that doesn’t have a particular coding skill, to do backtesting their strategy')
+    st.write('To this page. The Smirk is a Web page App that allows user, that doesn’t have a particular coding skill, to do backtesting their strategy')
