872beefa0799a5b3cfb36b3a6d469d9864c50e66
lia-defi <94789035+lia-defi@users.noreply.github.com>
2022-01-03 14:02:27 +0100

    Update multiapp.py

--- multiapp.py
+++ multiapp.py
@@ -1,11 +1,5 @@
 import streamlit as st
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden; }
-        footer {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 class MultiApp:
 
