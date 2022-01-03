2e0529235cfc14b67865c04c064a98497377b80e
lia-defi <94789035+lia-defi@users.noreply.github.com>
2022-01-03 13:56:29 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,11 @@
 import streamlit as st
-
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 def app():
     st.title('Welcome')
 
